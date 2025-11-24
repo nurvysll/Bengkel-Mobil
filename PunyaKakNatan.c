47c5ce4d539a44ad13dfb3852aaa82e2daa01e82
Gerrant Natanael <672022104@student.uksw.edu>
2025-11-24 22:05:03 +0700

    Update PunyaKakNatan.c

--- PunyaKakNatan.c
+++ PunyaKakNatan.c
@@ -899,6 +899,115 @@ void menuBendera() {
     pauseScreen();
 }
 
+void cetak_baris_bendera_animasi(int lebar, char *warna) {
+    printf("%s", warna);
+    fflush(stdout);
+
+    for (int j = 0; j < lebar; j++) {
+        printf("%c", KARAKTER_BENDERA);
+        fflush(stdout);
+        usleep(JEDA_MIKRODETIK);
+    }
+    printf("%s\n", RESET);
+    fflush(stdout);
+}
+
+void cetak_bendera_loading(int tinggi_input) {
+
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+
+    if (tinggi_input <= 0) tinggi_input = 1;
+
+    int tinggi_merah = tinggi_input;
+    int tinggi_putih = tinggi_input;
+
+    int tinggi_total_sebenarnya = tinggi_merah + tinggi_putih;
+
+
+    int lebar_bendera = tinggi_total_sebenarnya * LEBAR_MULTIPLIER / 2;
+
+    printf("================== Bendera Jaya ==================\n");
+    printf("Tinggi input: %d | Tinggi total: %d | Lebar: %d\n", tinggi_input, tinggi_total_sebenarnya, lebar_bendera);
+    printf("--- Merah: %d baris, Putih: %d baris ---\n", tinggi_merah, tinggi_putih);
+    printf("-------------------------------------------------------\n");
+
+
+    for (int i = 0; i < tinggi_merah; i++) {
+        cetak_baris_bendera_animasi(lebar_bendera, MERAH);
+    }
+
+
+    for (int i = 0; i < tinggi_putih; i++) {
+        cetak_baris_bendera_animasi(lebar_bendera, PUTIH);
+    }
+
+    printf("\n=======================================================\n");
+}
+
+
+
+
+void bersihkan_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main() {
+    int tinggi = 0;
+    char pilihan;
+
+    printf(">>> PROGRAM BENDERA MERAH PUTIH <<<\n\n");
+
+    do {
+        printf("Masukkan Tinggi Bendera : ");
+        if (scanf("%d", &tinggi) != 1) {
+            printf("Input Tinggi tidak valid. Coba lagi.\n");
+            bersihkan_input_buffer();
+            tinggi = 0;
+        }
+    } while (tinggi <= 0);
+
+    bersihkan_input_buffer();
+
+    do {
+        cetak_bendera_loading(tinggi);
+
+        printf("Masukkan pilihan (U: Ulang, X: Keluar): ");
+        if (scanf(" %c", &pilihan) != 1) {
+            bersihkan_input_buffer();
+            pilihan = ' ';
+        }
+
+        if (pilihan == 'U' || pilihan == 'u') {
+            bersihkan_input_buffer();
+
+             do {
+                printf("Masukkan Tinggi Per Bagian (Merah & Putih): ");
+                if (scanf("%d", &tinggi) != 1 || tinggi <= 0) {
+                    printf("Input Tinggi tidak valid atau nol. Coba lagi.\n");
+                    bersihkan_input_buffer();
+                }
+            } while (tinggi <= 0);
+            bersihkan_input_buffer();
+
+        } else if (pilihan == 'X' || pilihan == 'x') {
+            break;
+        } else {
+            printf("\nPilihan tidak dikenali. Tekan ENTER untuk kembali ke menu.\n");
+            bersihkan_input_buffer();
+            getchar();
+        }
+
+    } while (1);
+
+    printf("\nProgram selesai. Sampai jumpa!\n");
+    return 0;
+}
+
 
 // --- Fungsi Menu Utama (Navigator) ---
 
